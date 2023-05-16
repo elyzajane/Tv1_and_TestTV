3e0d41c7648f89ae4d307ba2aaad50565d784f71
Elyza <elyzajanerelucio@gmail.com>
2023-05-17 03:31:37 +0800

    Initialize the channel and volume

--- TV1&2.py
+++ TV1&2.py
@@ -1,4 +1,12 @@
 # Use def function to initialize the channel and volume
+    def __init__(self):
+        self.channel = 1
+        self.volumeLevel = 1
 # Method to turn off and on the tv
+    def turnOn(self):
+        self.on = True
+
+    def turnOff(self):
+        self.on = False
 # Set the channel
 # Set the volume of the channel
